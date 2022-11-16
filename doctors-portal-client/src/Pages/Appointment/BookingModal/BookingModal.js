7a2844c9f24dab20e31cf918bd4d854a665d0199
RaisulislamRimon <rimonppip0@gmail.com>
2022-11-16 09:45:11 +0600

    Modal data and state interactions

--- doctors-portal-client/src/Pages/Appointment/BookingModal/BookingModal.js
+++ doctors-portal-client/src/Pages/Appointment/BookingModal/BookingModal.js
@@ -3,6 +3,19 @@ import React from "react";
 
 const BookingModal = ({ treatment, selectedDate }) => {
   const { name, slots } = treatment;
+  const date = format(selectedDate, "PP");
+
+  const handleBooking = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const slot = form.slot.value;
+    const phone = form.phone.value;
+
+    console.log(name, email, slot, phone, date);
+  };
+
   return (
     <div>
       <input type="checkbox" id="booking-modal" className="modal-toggle" />
@@ -15,21 +28,37 @@ const BookingModal = ({ treatment, selectedDate }) => {
             ✕
           </label>
           <h3 className="text-lg font-bold">{name} </h3>
-          <form className="grid grid-cols-1 gap-3 mt-10">
+          <form
+            onSubmit={handleBooking}
+            className="grid grid-cols-1 gap-3 mt-10"
+          >
             <input
               type="text"
               value={format(selectedDate, "PP")}
               disabled
               className="input w-full input-bordered "
             />
-            <select className="select select-bordered w-full">
+            <select name="slot" className="select select-bordered w-full">
               {slots.map((slot) => (
                 <option value={slot}>{slot}</option>
               ))}
             </select>
             <input
               type="text"
-              placeholder="Type here"
+              name="name"
+              placeholder="Your Name"
+              className="input w-full input-bordered "
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="Email address"
+              className="input w-full input-bordered "
+            />
+            <input
+              type="text"
+              name="phone"
+              placeholder="Phone Number"
               className="input w-full input-bordered "
             />
             <br />
